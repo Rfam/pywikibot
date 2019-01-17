6285d069b3c6768d68c444f38edba4c3a1a86604
Anton Petrov <apetrov@ebi.ac.uk>
2019-01-17 13:56:31 +0000

    Add a script for adding infoboxes to wikipedia

---
+++ scripts/userscripts/add_infobox.py
@@ -0,0 +1,52 @@
+"""
+Script for adding Rfam infoboxes to Wikipedia pages.
+
+Usage:
+python pwb.py add_infobox RF03114 RF03115
+"""
+
+import sys
+
+import pywikibot
+
+from generate_infobox import get_infobox
+from rfam_db import connection
+
+
+def add_infobox(family):
+    site = pywikibot.Site('en', 'wikipedia')
+    infobox = get_infobox(family)
+
+    with connection.cursor() as cursor:
+        sql = """SELECT title
+                 FROM wikitext, family
+                 WHERE
+                 wikitext.auto_wiki = family.auto_wiki
+                 AND family.rfam_acc = '{}'""".format(family)
+        cursor.execute(sql)
+        result = cursor.fetchone()
+
+    wiki = pywikibot.Page(site, result['title'].decode('utf-8'))
+
+    if 'Infobox' in wiki.text:
+        print('Infobox already exists, not adding a new one')
+        return
+
+    wiki.text = infobox + '\n' + wiki.text
+    print wiki.text
+    # wiki.save('Add Rfam infobox', botflag=True)
+
+
+def main():
+    if len(sys.argv) > 1:
+        families = sys.argv[1:]
+    else:
+        print 'Please specify Rfam accession'
+        return
+
+    for family in families:
+        add_infobox(family)
+
+
+if __name__ == '__main__':
+    main()
